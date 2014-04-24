2ce639505a1bd6b5f09ae4bf54d44a98595ef463
marco <mpao@me.com>
2014-04-24 17:05:13 +0200

    La board viene inserita all'interno di un layout più complesso ( in futuro eh, ora è vuoto )

--- src/io/github/mpao/magicsquare/MainActivity.java
+++ src/io/github/mpao/magicsquare/MainActivity.java
@@ -1,7 +1,12 @@
 package io.github.mpao.magicsquare;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,13 @@ public class MainActivity extends Activity {
 		
 		SquareLayout board = new SquareLayout(this);
 		board.setBackgroundResource(R.color.board);
-		setContentView(board);
+		
+		/* http://developer.android.com/reference/android/view/LayoutInflater.html
+		 * Carico e scompatto il layout principale */
+		LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View mainLayout = inflater.inflate(R.layout.activity_main, null);
+		/* Attenzione al CAST: nel main uso LinearLayout, dopo di che, aggiungo la board */
+		((LinearLayout) mainLayout).addView(board);
+		setContentView(mainLayout);
 	}
 }
