a3add948a2fa1b138396d4994d9f47ad7e5d96e4
marco <mpao@me.com>
2014-07-15 16:27:04 +0200

    no message

--- src/io/github/mpao/magicsquare/MainActivity.java
+++ src/io/github/mpao/magicsquare/MainActivity.java
@@ -13,19 +13,20 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().hide(); // nascondo l'action bar, non mi serve in questa app
 		/* creo l'oggetto che conterrà le mie caselle */
 		SquareLayout board = new SquareLayout(this);
 		/* e ne imposto il colore come definito in color.xml*/
 		board.setBackgroundResource(R.color.board);
-		
 		/* http://developer.android.com/reference/android/view/LayoutInflater.html
 		 * Carico e scompatto il layout principale */
 		LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View mainLayout = inflater.inflate(R.layout.activity_main, null);
+		View mainLayout = inflater.inflate(R.layout.activity_main, board);
 		/* Attenzione al CAST: nel main uso LinearLayout, dopo di che, aggiungo la board */
 		/* ADDVIEW: Parameters: child the child view to add - index the position at which to add the child
 		 * aggiungendo un index quindi posso inserire la view in qualunque posizione del layout */
-		((LinearLayout) mainLayout).addView(board); //inserire un indice quando il layout sarà completo
+		//((LinearLayout) mainLayout).addView(board); //inserire un indice quando il layout sarà completo
+		
 		/* assegno il layout all'activity */
 		setContentView(mainLayout);
 	}
