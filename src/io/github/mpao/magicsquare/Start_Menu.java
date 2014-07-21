1b26a5f69f23da6063fca7f28d634a34a4bb90fd
marco <mpao@me.com>
2014-07-21 17:08:07 +0200

    Comincio la scrittura del metodo startGame per verificare esistenza di partite in corso

--- src/io/github/mpao/magicsquare/Start_Menu.java
+++ src/io/github/mpao/magicsquare/Start_Menu.java
@@ -1,7 +1,9 @@
 package io.github.mpao.magicsquare;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,6 +18,16 @@ public class Start_Menu extends Activity {
 	public void startGame(View view) {
 		Intent intent = new Intent(this, MainActivity.class);
 		intent.putExtra(MESSAGE, 0);
+		/*  se clicco su start un dialog mi deve avvertire se esiste una partita in corso
+			- Una partita in corso esiste, se esiste SharedPreferences e result > 0 
+			- SharedPreferences esiste se una partita iniziata ha result > 0 e non è finita
+			- Una partita è finita quando non ho più mosse o sono arrivato a 100 */
+		SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+		if(sharedPref.contains("punteggio") && sharedPref.getInt("punteggio", 0)>0){
+			/* se esiste "punteggio" e (lazy) se esiste è maggiore di 0 
+			 * allora mostrami un alert che mi dice se sono sicuro di perdere i 
+			 * progressi dell'altra partita */
+		}
 		startActivity(intent);
 	}	
 	public void resumeGame(View view) {
