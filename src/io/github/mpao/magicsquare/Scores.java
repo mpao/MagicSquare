0b2f2eee31010bad6576aa43b9dcba6d82fdbdc2
marco <mpao@me.com>
2014-08-01 13:15:34 +0200

    il metodo timeToString era completamente sbagliato. Corretto in questo commit; ridisegnato l'activity Score ora pronta per i dati dal DB ( questa volta per davvero -.-' )

--- src/io/github/mpao/magicsquare/Scores.java
+++ src/io/github/mpao/magicsquare/Scores.java
@@ -7,10 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Scores extends Activity {
@@ -19,7 +16,11 @@ public class Scores extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().hide(); // nascondo l'action bar, non mi serve in questa app
-		/* Questa activity viene creata in due occasioni:
+		setContentView(R.layout.scores);
+	}
+	@Override
+	protected void onStart(){
+		/* Questa activity viene lanciata in due occasioni:
 		 * 1. quando finisce una partita
 		 * 2. dal menù iniziale, cliccando sul pulsante per vedere la classifica.
 		 * Nel primo caso, viene passato un intent che contiene il punteggio e il tempo della
@@ -49,22 +50,14 @@ public class Scores extends Activity {
          * Inoltre devo inserire le modalità in cui è stato ottenuto il punteggio, ovvero se l'help era attivo
          * o meno e se era una partita basic o expert */
         }
-        // rappresentazione provvisoria
-		LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		/* utilizzo ancora LayoutInflater invece che un layout statico in XML per inserire il messaggio,
-		 * ma credo che in layout definitivo possa trovare posto normalmente */
-		ViewGroup root = (ViewGroup)findViewById(R.layout.scores);
-		View mainLayout = inflater.inflate(R.layout.scores, root);
-
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        String test = "Punteggio = "+ punteggio.toString() + " tempo = "+ timeToString(tempo)+"\n";
-        test = test + "punti = " + calcolaPunti(punteggio, tempo);
-        textView.setText(test);           
-        ((LinearLayout) mainLayout).addView(textView,0);		
-		setContentView(mainLayout);
+        /* questa view mi serve per scrivere messaggio di congratulazioni o altro quando si arriva 
+         * nella activity dopo la partita. Se si arriva qui dal menù che ci metto ? La parola CLASSIFICA ? */
+        TextView message = (TextView)findViewById(R.id.scoreMessage);
+        message.setText(this.debugPunteggio(punteggio, tempo));
+        /* view con i migliori 10 punteggi.*/
+        TextView scores = (TextView)findViewById(R.id.scoresList);
+		super.onStart();
 	}
-	
 	public void tornaAlMenu(View v){
 		/* metodo del pulsante per chiudere l'activity e tornare al menù */
 		startActivity(new Intent(this, Start_Menu.class));
@@ -86,6 +79,7 @@ public class Scores extends Activity {
 		 * dello stesso punteggio, quindi diventa inversamente proporzionale al quadrato
 		 * del tempo: stesso punteggio, ma un secondo migliore ha un gain notevole!
 		 * Così facendo però i numeri sono di diversi ordini di grandezza differenti */
+
 		punti = (int)((float)(p * ORDINEGRANDEZZA / (t*t) ));
 		// bonus se raggiungi 100
 		if(p==100) punti += BONUS;
@@ -95,11 +89,23 @@ public class Scores extends Activity {
 		if(p<70) punti /= 10000 ;
 		return punti;
 	}
-	public String timeToString(Long l){
-		//restituisce una stringa formattata come 1'21"046 in base al tempo parametro
-		long minuti   = TimeUnit.MILLISECONDS.toMinutes(l);
-		long secondi  = TimeUnit.MILLISECONDS.toSeconds(l);
-		long millisec = TimeUnit.MILLISECONDS.toMillis(l/10);
+	public String timeToString(Long t){
+		/*restituisce una stringa formattata come 1'21"046 in base al tempo parametro.
+		 * TimeUnit non è tanto semplice, poteva essere qualcosa di meglio. Tanto valeva 
+		 * fare i calcoli sul long: ad esempio 132984 tolgo le ultime 3 cifre che sono i ms
+		 * e divido per 1000. Rimane 132 che sono 2 minuti e 12 secondi. TimeUnit è bello 
+		 * da vedere e pulito, ma ci si incasina non poco */
+		long minuti   = TimeUnit.MILLISECONDS.toMinutes(t);
+		long secondi  = TimeUnit.MILLISECONDS.toSeconds(t) - 
+						TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(t));
+		long millisec = t-TimeUnit.MILLISECONDS.toSeconds(t)*1000;		
 		return String.format("%d'%d''%d", minuti, secondi, millisec);
+	}
+	private String debugPunteggio(Integer p, Long t){
+		String test = "Punteggio = "+ p.toString() +"\n";
+        test += "tempo = "+ timeToString(t)+"\n";
+        test += "tempo_ms = "+ t.toString() +"\n";
+        test += "punti = " + calcolaPunti(p, t)+"\n";
+        return test;
 	}
 }
